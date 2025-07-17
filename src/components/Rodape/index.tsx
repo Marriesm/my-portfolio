4c023e236cdfadf66cf881aa56f087067d165078
Marriesm <ms-eduardasoares@outlook.com>
2025-07-17 09:43:19 -0300

    Acabei corrigindo cores

--- src/components/Rodape/index.tsx
+++ src/components/Rodape/index.tsx
@@ -11,10 +11,10 @@ const Rodape = () => {
             </div>
             
             <div className={styles.footer__icons}>
-                <FaGithub size={25} color="#fffff"/> 
-                <FaLinkedin size={25} color="#fffff"/>
-                <FaDiscord size={25} color="#fffff"/>
-                <FaInstagram size={25} color="#fffff"/>
+                <FaGithub size={25} className={styles.footer__icon}/> 
+                <FaLinkedin size={25} className={styles.footer__icon}/>
+                <FaDiscord size={25} className={styles.footer__icon}/>
+                <FaInstagram size={25}className={styles.footer__icon}/>
                 
             </div>
             <p className={styles.footer__desc}>2025 © Maria Eduarda Soares. All rights reserved</p>
